f074f586bc407a29b4dbf0b776de5154e9cbe581
Alexander Schier <allo@laxu.de>
2016-06-11 13:40:17 +0200

    fixed url_params for string input (or empty input)

--- feedjack/templatetags/urlparams.py
+++ feedjack/templatetags/urlparams.py
@@ -7,6 +7,13 @@ def urlparams(url_params, exclude):
     # as & is the separator in urls, too.
     exclude = exclude.split("&")
     result=""
+    if isinstance(url_params, basestring):
+        params = url_params.split("&")
+        url_params = {}
+        for param in params:
+            parts = param.split("=")
+            if len(parts) == 2:
+                url_params[parts[0]] = parts[1]
     for key, values in url_params.items():
         if key in exclude:
             continue
